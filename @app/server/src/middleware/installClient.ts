c9ccb0dfec83d5e081b60d29525610fa3beee7a4
Rudin Swagerman <post@rudinswagerman.nl>
2019-12-07 16:56:57 +0100

    added proxy for client development

--- @app/server/src/middleware/installClient.ts
+++ @app/server/src/middleware/installClient.ts
@@ -1,17 +1,23 @@
 import express, { Express } from "express";
+import proxy from "http-proxy-middleware"
 
 if (!process.env.NODE_ENV) {
   throw new Error("No NODE_ENV envvar! Try `export NODE_ENV=development`");
 }
 
-// const isDev = process.env.NODE_ENV !== "production";
+const isDev = process.env.NODE_ENV !== "production";
 
 export default async function installClient(app: Express) {
 
-  app.use(express.static(`${__dirname}/../../../client/build`))
+  if (isDev) {
+    const apiProxy = proxy('http://localhost:3000', {ws: true});
+    app.use('/', apiProxy);
+  } else {
+    app.use(express.static(`${__dirname}/../../../client/build`))
 
-  app.get("*", function(_req, res) {
-    res.sendFile(`${__dirname}/../../../client/build`, "index.html");
-  });
+    app.get("*", function(_req, res) {
+      res.sendFile(`${__dirname}/../../../client/build`, "index.html");
+    });
+  }
 
 }
